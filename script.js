33b23cf2ef3c3128d1502a48668af7590891816e
Vinícius Gonzaga Guilherme <viniciusgonzagaguilherme@gmail.com>
2022-08-24 11:46:43 -0300

    Atualização em diversas funcionalidades HTML, JS

--- script.js
+++ script.js
@@ -1,43 +1,85 @@
-
-let add = function(){
+let loadPage = function(){
     let db = JSON.parse(localStorage.getItem("db_todo") || "[]");
-
-    let text = document.getElementById("to-do").value;
     let container = document.getElementById("div-container");
-    container.innerHTML += `
+    container.innerHTML = "";
+    db.forEach(item =>{
+        if(item.check == true){
+            container.innerHTML += `
                 <div class="item">
-                    <div class="div-input" id="checkbox1">
-                        <div class="div-checkbox" onclick="check()"></div>    
+                    <div class="div-input" id="${item.id}">
+                        <div class="div-checkbox" style="background-color: gray;" onclick="uncheck('${item.id}')"><img src="./Images/checkicon.png" width="20px"></div>    
                     </div>
                     <div class="div-content">
-                        ${text}
+                        ${item.content}
                     </div>
                     <div class="div-info">
                         <img class="option" src="./Images/option.png" width="20px">
                     </div>
                 </div>
-    `
-    console.log(db);
-    console.log(db.lenght);
+                `
+        }
+        else{
+            container.innerHTML += `
+                <div class="item">
+                    <div class="div-input" id="${item.id}">
+                        <div class="div-checkbox" onclick="check('${item.id}')"></div>    
+                    </div>
+                    <div class="div-content">
+                        ${item.content}
+                    </div>
+                    <div class="div-info">
+                        <img class="option" src="./Images/option.png" width="20px">
+                    </div>
+                </div>
+                `
+        }
+        
+    })
+
+    localStorage.setItem("db_todo", JSON.stringify(db));
+}
+
+loadPage();
+
+
+let add = function(){
+    let db = JSON.parse(localStorage.getItem("db_todo"));
+
+    let text = document.getElementById("to-do").value;
     let item = {
-        id: db.lenght,
-        content: text
+        id: `checkbox${db.length + 1}`,
+        content: text,
+        check: false,
     }
 
     db.push(item);
-
     localStorage.setItem("db_todo", JSON.stringify(db));
-    console.log(text);
+
+    loadPage();
 
 }
 
-let check = function(){
-    console.log("teste");
-    let item = document.getElementById("checkbox1");
-    item.innerHTML = `<div class="div-checkbox" style="background-color: gray;" onclick="uncheck()"><img src="./Images/checkicon.png" width="20px"></div>`
+let check = function(aux){
+    let db = JSON.parse(localStorage.getItem("db_todo"));
+    db.forEach(data =>{
+        if(data.id == aux)
+            data.check = true;
+    })
+    localStorage.setItem("db_todo", JSON.stringify(db));
+
+    let item = document.getElementById(aux);
+    item.innerHTML = `<div class="div-checkbox" style="background-color: gray;" onclick="uncheck('${aux}')"><img src="./Images/checkicon.png" width="20px"></div>`
+    
 }
 
-let uncheck = function(){
-    let item = document.getElementById("checkbox1");
-    item.innerHTML = `<div class="div-checkbox" onclick="check()"></div> `
+let uncheck = function(aux){
+    let db = JSON.parse(localStorage.getItem("db_todo"));
+    db.forEach(data =>{
+        if(data.id == aux)
+            data.check = false;
+    })
+    localStorage.setItem("db_todo", JSON.stringify(db));
+
+    let item = document.getElementById(aux);
+    item.innerHTML = `<div class="div-checkbox" onclick="check('${aux}')"></div> `
 }
