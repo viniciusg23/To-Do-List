ee63d09de5f9732ddd68e0aa6389e856d312196a
Vinícius Gonzaga Guilherme <viniciusgonzagaguilherme@gmail.com>
2022-08-28 18:15:29 -0300

    finalização da função add()

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let loadPage = function(){
     let text = document.getElementsByClassName('to-do');
-    text.value = '';
+    text.value = "";
+    document.getElementsByName('todo')[0].placeholder="To-do";
     let db = JSON.parse(localStorage.getItem("db_todo") || "[]");
     let container = document.getElementById("div-container");
     container.innerHTML = "";
@@ -9,13 +10,13 @@ let loadPage = function(){
             container.innerHTML += `
                 <div class="item">
                     <div class="div-input" id="${item.id}">
-                        <div class="div-checkbox" style="background-color: gray;" onclick="uncheck('${item.id}')"><img src="./Images/checkicon.png" width="20px"></div>    
+                        <div class="div-checkbox" style="background-color: #607EAA;" onclick="uncheck('${item.id}')"><img src="./Images/checkicon.png" width="20px"></div>    
                     </div>
                     <div class="div-content">
                         ${item.content}
                     </div>
                     <div class="div-info">
-                        <img class="option" src="./Images/option.png" width="20px">
+                        <img class="option" src="./Images/option.png" width="20px" id="info-${item.id}">
                     </div>
                 </div>
                 `
@@ -30,7 +31,7 @@ let loadPage = function(){
                         ${item.content}
                     </div>
                     <div class="div-info">
-                        <img class="option" src="./Images/option.png" width="20px">
+                        <img class="option" src="./Images/option.png" width="20px" id="info-${item.id}">
                     </div>
                 </div>
                 `
@@ -47,40 +48,25 @@ loadPage();
 let add = function(){
     let db = JSON.parse(localStorage.getItem("db_todo"));
 
-    let form = document.getElementById("div-form");
-    // let text = document.getElementsByClassName('to-do');
-    let textContent = document.getElementById("to-do-content");
-    let text = document.getElementsByName('todo')[0].placeholder="Não se esqueça da tarefa ;)";
-    console.log(textContent.value);
-    if(textContent.value != ""){
-        
+    let text = document.getElementById('to-do');
+
+    if(text.value != ""){
+        let item = {
+            id: `checkbox${db.length + 1}`,
+            content: text.value,
+            check: false,
+        }
+
+        db.push(item);
+        localStorage.setItem("db_todo", JSON.stringify(db));
+
+        loadPage();  
+        text.value = "";
     }
-
-    //     console.log("Entrou if")
-    //     let item = {
-    //         id: `checkbox${db.length + 1}`,
-    //         content: textContent.value,
-    //         check: false,
-    //     }
-    
-    //     db.push(item);
-    //     localStorage.setItem("db_todo", JSON.stringify(db));
-        
-    //     form.innerHTML = `
-    //                         <input type="text" name="to-do" class="to-do" id="to-do-content" placeholder="To-do" maxlength="128"></input>
-    //                         <div id="add" onclick="add()"><b>ADICIONAR</b></div>
-    //                     `
-
-    //     loadPage();    
-    // }
-    // else{
-    //     form.innerHTML = `
-    //                         <input type="text" name="to-do" class="to-do" id="to-do-null" placeholder="Não se esqueça da tarefa ;)" maxlength="128">
-    //                         <div id="add" onclick="add()"><b>ADICIONAR</b></div>
-    //                     `
-    //     let text = document.getElementsByClassName('to-do');
-    //     text.focus();
-    // }
+    else{
+        document.getElementsByName('todo')[0].placeholder="Não se esqueça da tarefa ;)";
+        text.focus();
+    }
     
 }
 
