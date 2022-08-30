ce4f26cf14f71f8a80ffab1b9ee940410b9deb36
Vinícius Gonzaga Guilherme <viniciusgonzagaguilherme@gmail.com>
2022-08-30 18:13:56 -0300

    Adição da função de Editar Terefa

--- script.js
+++ script.js
@@ -89,16 +89,57 @@ let cancel = function(){
     let container = document.getElementById("container");
     let edit3 = document.getElementById("edit");
     let delet = document.getElementById("delete");
+    let div = document.getElementById("div-buttons");
 
+    div.innerHTML = `
+        <div class="button cancel-edit" onclick="cancel()"><p>Cancelar</p></div>
+        <div class="button cancel-edit" id="edit"><p>Editar</p></div>
+        <div class="button" id="delete"><p>Excluir</p></div>
+    `
     menu.setAttribute("style", "display: none;")
     container.setAttribute("style", "filter:blur(0px);")
     container.removeAttribute("onclick");
-    edit3.removeAttribute("onclick");
-    delet.removeAttribute("onclick");
+    try{
+        edit3.removeAttribute("onclick");
+        delet.removeAttribute("onclick");
+    }
+    catch{}
+    
 }
-let edit = function(){
+let edit = function(item){
+    let div = document.getElementById("div-buttons");
+    div.innerHTML = `
+        <div id="div-edit">
+            <input type="text" id="text-edit" placeholder="Digite aqui sua alteração...">
+            <div class="edit-button" onclick="editCancel('${item}')"><p>Cancelar</p></div>
+            <div class="edit-button" id="save-edit" onclick="editSave('${item}')"><b>Salvar</b></div>
+        </div>
+    `
+}
+let editSave = function(item){
+    let db = JSON.parse(localStorage.getItem("db_todo"));
+    let text = document.getElementById("text-edit");
+    db.forEach(task =>{
+        if(task.id == item){
+            task.content = text.value;
+        }
+    })
+    localStorage.setItem("db_todo", JSON.stringify(db));
+    
+    cancel();
+    loadPage();
+}
 
-}
+let editCancel = function(item){
+    let div = document.getElementById("div-buttons");
+    div.innerHTML = `
+        <div class="button cancel-edit" onclick="cancel()"><p>Cancelar</p></div>
+        <div class="button cancel-edit" id="edit" onclick="edit('${item}')"><p>Editar</p></div>
+        <div class="button" id="delete" onclick="delet3('${item}')"><p>Excluir</p></div>
+    `
+} 
+
+
 let delet3 = function(item){
     let db = JSON.parse(localStorage.getItem("db_todo"));
 
